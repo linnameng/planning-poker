efc06bbcf9f1a761a514fff628a5dcbfc6c00c9d
Linna Meng <mengl@mac-2387.local>
2019-05-22 11:40:34 -0400

    LM Added login page, started list page

--- src/Login.js
+++ src/Login.js
@@ -4,10 +4,30 @@ import './Login.css';
 function Login() {
   return (
     <div className="Login">
-      <header className="Login-header">
-        Login page goes here
-      </header>
-    </div>
+      <div className="Login-form-container">
+        <div className="uk-align-center Login-form">
+        <span uk-icon="icon: users; ratio: 5" className="uk-margin"></span>
+        <form>
+          <fieldset className="uk-fieldset">
+            <div className="uk-margin">
+              <div className="uk-inline">
+                <span className="uk-form-icon" uk-icon="icon: user"></span><input className="uk-input" type="text" placeholder="Username"></input>
+              </div>
+            </div>
+            <div className="uk-margin">
+              <div className="uk-inline">    
+                <span className="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                <input className="uk-input" type="text" placeholder="Password"></input>
+              </div>
+            </div>
+          </fieldset>
+          <div className="uk-inline">
+            <button className="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Login</button>
+          </div>
+        </form>
+        </div>
+      </div>
+     </div>
   );
 }
 
