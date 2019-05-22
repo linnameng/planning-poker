d5264addbb1ae2f4036046827cacf4c1fa0124dc
Linna Meng <mengl@mac-2387.local>
2019-05-22 10:30:21 -0400

    LM started creating login page

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        Login page goes here
+      </header>
+    </div>
+  );
+}
+
+export default Login;
